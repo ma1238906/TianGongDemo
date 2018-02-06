25a1f76748f0fffa1196615975bb3c2e0a757b19
Ma Junpeng <373834486@qq.com>
2018-02-06 11:00:04 +0800

    天宫一号 全息视频录屏

---
+++ Assets/ProjectAssets/Scripts/InverseCamera.shader
@@ -0,0 +1,58 @@
+﻿Shader "Custom/InverseCamera" {
+	Properties{
+		_MainTex("Base (RGB)", 2D) = "" {}
+	_AxisX("x or y", Range(0,1)) = 0 //0 for x; 1 for y  
+	}
+
+		// Shader code pasted into all further CGPROGRAM blocks  
+		CGINCLUDE
+
+#include "UnityCG.cginc"  
+
+		struct v2f {
+		float4 pos : SV_POSITION;
+		float2 uv : TEXCOORD0;
+	};
+
+	sampler2D _MainTex;
+	half4 _MainTex_ST;
+
+	float _AxisX;
+
+	v2f vert(appdata_img v)
+	{
+		v2f o;
+		o.pos = UnityObjectToClipPos(v.vertex);
+		o.uv = v.texcoord.xy;
+		return o;
+	}
+
+	half4 frag(v2f i) : SV_Target
+	{
+		if (_AxisX == 0)
+		i.uv.x = 1 - i.uv.x;
+		else
+			i.uv.y = 1 - i.uv.y;
+
+	half4 color = tex2D(_MainTex, UnityStereoScreenSpaceUVAdjust(i.uv, _MainTex_ST));
+
+	return color;
+	}
+
+		ENDCG
+
+		Subshader {
+		Pass{
+			ZTest Always Cull Off ZWrite Off
+
+			CGPROGRAM
+#pragma vertex vert  
+#pragma fragment frag  
+			ENDCG
+		}
+
+	}
+
+	Fallback off
+
+} // shader 
---
+++ Assets/ProjectAssets/Scripts/Mirror.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class Mirror : MonoBehaviour
+{
+    public bool NeedRevert = false;
+    public Shader invertShader;
+    private bool isInit;
+
+    void Start()
+    {
+        invertShader = Shader.Find("Custom/InverseCamera");
+        isInit = true;
+    }
+
+    static Material m_Material = null;
+    protected Material material
+    {
+        get
+        {
+            if (m_Material == null)
+            {
+                m_Material = new Material(invertShader);
+                m_Material.hideFlags = HideFlags.DontSave;
+            }
+            return m_Material;
+        }
+    }
+
+    protected void OnDisable()
+    {
+        if (m_Material)
+        {
+            DestroyImmediate(m_Material);
+        }
+    }
+
+    // Called by the camera to apply the image effect  
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (NeedRevert && isInit)
+        {
+
+            if (Screen.orientation == ScreenOrientation.Portrait || Screen.orientation == ScreenOrientation.PortraitUpsideDown)
+            {
+                material.SetFloat("_AxisX", 0);
+            }
+            else
+            {
+                material.SetFloat("_AxisX", 1);
+            }
+
+            Graphics.Blit(source, destination, material);
+        }
+        else Graphics.Blit(source, destination);
+    }
+}
